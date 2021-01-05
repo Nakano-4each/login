3fa405238b10b90bec029397653186b6cf29fbfa
Nakano-4each <0sb3263t1bf431k68@gmail.com>
2021-01-05 11:06:59 +0900

    アップロード日2021/01/05

---
+++ register_confirm.php
@@ -0,0 +1,56 @@
+<?php
+
+$temp_pic_name = $_FILES['picture']['tmp_name'];
+
+$original_pic_name = $_FILES['picture']['name'];
+$path_filename = './image/'.$original_pic_name;
+
+move_uploaded_file($temp_pic_name,'./image/'.$original_pic_name);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>会員登録確認</title>
+        <link rel="stylesheet" type="text/css" href="register_confirm_.css">
+    </head>
+
+<body>
+    <header>
+        <img src="4eachblog_logo.jpg">
+    </header>
+    
+    <main>
+        <h2>会員登録 確認</h2>
+        <div class="a">こちらの内容で登録しても宜しいでしょうか?</div>
+        <p>氏名 : <?php echo $_POST['name']; ?></p>
+        <p>メール : <?php echo $_POST['mail']; ?></p>
+        <p>パスワード : <?php echo $_POST['password']; ?></p>
+        <p>プロフィール写真 : <?php echo $_FILES['picture']['name']; ?></p>
+        <p>コメント : <?php echo $_POST['comments']; ?></p>
+        
+        <box>
+        <form action="register.php">
+            <input type="submit" class="button1" value="戻って修正する"/>
+        </form>
+        
+        <form action="register_insert.php" method="post">
+            <input type="submit" class="button2" value="登録する"/>
+            <input type="hidden" value="<?php echo $_POST['name']; ?>" name ="name">
+            <input type="hidden" value="<?php echo $_POST['mail']; ?>" name ="mail">
+            <input type="hidden" value="<?php echo $_POST['password']; ?>" name ="password">
+            <input type="hidden" value="<?php echo $path_filename; ?>" name ="path_filename">
+            <input type="hidden" value="<?php echo $_POST['comments']; ?>" name ="comments">
+        </form>
+        </box>
+    </main>
+
+    <footer>
+        © 2018 InterNous.inc. All rights reserved
+    </footer>
+</body>
+
+</html>
